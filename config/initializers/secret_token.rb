125f160908eb842d65f1c9a978cab5525e02851c
Brandon Elder <brandonelder3@gmail.com>
2017-02-05 15:00:06 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DigIt::Application.config.secret_key_base = '9ebedf1b03eb93493d753a4322c7f1f0662e9271cf8ef3ef83961dcbefe74ffb5e7ec283febc255e839e7090c30475cf8e65ede9ee74b17eb340d7285ff95191'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DigIt::Application.config.session_store :cookie_store, key: '_dig-it_session'

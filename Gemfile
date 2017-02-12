97c4b7d96c2dd8d5990c3af6182b0d3b4b71bf18
Brandon Elder <brandonelder3@gmail.com>
2017-02-12 13:20:53 +0000

    added 12Factor gem and changed compile assets to true

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'rails_12factor', group: :production
+
 gem 'bootstrap-sass', '~> 3.3.5.1'
 
 gem 'sprockets', '2.11.0'
--- app/assets/javascripts/ebay.js
+++ app/assets/javascripts/ebay.js
@@ -1,34 +1,40 @@
-$(function() {
-  url = "http://svcs.ebay.com/services/search/FindingService/v1";
-  url += "?OPERATION-NAME=findCompletedItems";
-  url += "&SERVICE-VERSION=1.13.0";
-  url += "&SERVICE-NAME=FindingService";
-  url += "&SECURITY-APPNAME=BrandonE-DigIt-PRD-5cd429718-3d6a116b";
-  url += "&GLOBAL-ID=EBAY-US";
-  url += "&RESPONSE-DATA-FORMAT=JSON";
-  // url += "&callback=callbackfunc"; //optional?
-  url += "&REST-PAYLOAD";
-  url += "&paginationInput.pageNumber=1";
-  url += "&paginationInput.entriesPerPage=10";
-  url += "&keywords=rare soul 45";
-  url += "&sortOrder=StartTimeNewest";
+$(window).load(function() {
+  $('form[role="search"]').submit(function(ev)  {
+    ev.preventDefault();
+    // get the input value with:
+    var searchstring = $('input[type="text"]', this).val();
+    //console.log(searchstring);
+    // your ajax request, using the variable above
+    var url = "http://svcs.ebay.com/services/search/FindingService/v1";
+    url += "?OPERATION-NAME=findCompletedItems";
+    url += "&SERVICE-VERSION=1.13.0";
+    url += "&SERVICE-NAME=FindingService";
+    url += "&SECURITY-APPNAME=BrandonE-DigIt-PRD-5cd429718-3d6a116b";
+    url += "&GLOBAL-ID=EBAY-US";
+    url += "&RESPONSE-DATA-FORMAT=JSON";
+    url += "&REST-PAYLOAD";
+    url += "&paginationInput.pageNumber=1";
+    url += "&paginationInput.entriesPerPage=25";
+    url += "&keywords=" + searchstring;
+    url += "&sortOrder=StartTimeNewest";
 
     $.ajax({
-        type: "GET",
-        url: url,
-        dataType: "jsonp",
-        success: function(res){
-          console.log(res);
-          var items = res.findCompletedItemsResponse[0].searchResult[0].item;
-          var ins = "";
-          for (var i = 0; i < items.length; i++){
-            ins += "<div>";
-            ins += "<img src='" + items[i].galleryURL + "  '/>";
-            ins += "  " + items[i].title + " - ";
-            ins += "Sold for $" + items[i].sellingStatus[0].currentPrice[0].__value__;
-            ins += "</div><br />";
-          };
-          $('.results').html(ins);
-        }
+      type: "GET",
+      url: url,
+      dataType: "jsonp",
+      success: function(res){
+        console.log(res);
+        var items = res.findCompletedItemsResponse[0].searchResult[0].item;
+        var ins = "";
+        for (var i = 0; i < items.length; i++){
+          ins += "<div>";
+          ins += "<img src='" + items[i].galleryURL + "  '/>";
+          ins += "  " + items[i].title + " - ";
+          ins += "Sold for $" + items[i].sellingStatus[0].currentPrice[0].__value__;
+          ins += "</div><br />";
+        };
+        $('.results').html(ins);
+      }
     });
+  });
 });
